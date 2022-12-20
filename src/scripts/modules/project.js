5a12e36dc1636d1a07895fbf1edf402062b3e338
adaboh <kweada@gmail.com>
2022-12-20 05:36:18 -0500

    edited about and resume pages

--- src/scripts/modules/project.js
+++ src/scripts/modules/project.js
@@ -16,3 +16,23 @@ if (!homePage) {
 }
 
 // console.log("webpack compiled successfully");
+
+//! Read More - About page code
+const buttonReadMore = document.querySelector(".button-readmore");
+// const aboutMoreText = document.querySelector(".about__readmore");
+let aboutMoreText = document.getElementById("about__readmore");
+
+// console.log(aboutMoreText);
+
+buttonReadMore.addEventListener("click", function () {
+  if (aboutMoreText.style.display === "none") {
+    buttonReadMore.textContent = "Read Less";
+    aboutMoreText.style.display = "block";
+  } else {
+    buttonReadMore.textContent = "Read More";
+    aboutMoreText.style.display = "none";
+  }
+});
+//*[@id="landmark-main"]/div/div[1]/div[7]/div[1]/div/div[3]/div/span/div/text()
+
+// 1768 E 24th Ave, 43219
